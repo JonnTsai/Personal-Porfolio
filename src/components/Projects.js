a84061c8ad71a75e05a33ea723bcbe269e567fdc
Jonathan Tsai <jonnsighed95@gmail.com>
2024-03-01 22:44:15 -0800

    Projects portion initialization

---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Projects = () => {
+  return (
+    <section className='project' id='projects'>
+        <p>test</p>
+    </section>
+  )
+}
+
+export default Projects
