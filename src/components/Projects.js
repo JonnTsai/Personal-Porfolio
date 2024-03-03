9570e46fcc6f4d075c9265da708e87d127100464
Jonathan Tsai <jonnsighed95@gmail.com>
2024-03-03 11:32:04 -0800

    Projects and projectcards

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,88 @@
 import React from 'react'
+import { Container, TabContent, Col, Row, Tab, Nav } from 'react-bootstrap';
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from '../assets/img/project-img1.png'
+import projImg2 from '../assets/img/project-img2.png'
+import projImg3 from '../assets/img/project-img3.png'
+import colorSharp2 from "../assets/img/color-sharp2.png"
 
 export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
   return (
     <section className='project' id='projects'>
-        <p>test</p>
+        <Container>
+          <Row>
+            <Col>
+              <h2>Projects</h2>
+              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+              <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                <Nav variant="pills" defaultActiveKey="/home">
+                  <Nav.Item>
+                    <Nav.Link eventKey="first">Tab One</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="second">Tab two</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="third">Tab Three</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+                <TabContent>
+                  <Tab.Pane eventKey="first">
+                    <Row>
+                      {
+                        projects.map((project, index) => {
+                          return (
+                            <ProjectCard
+                              key={index}
+                              {...project}
+                            />
+                          )
+                        })
+                      }
+                    </Row>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="second">Loren ipsum</Tab.Pane>
+                  <Tab.Pane eventKey="third">Loren ipsum</Tab.Pane>
+                </TabContent>
+              </Tab.Container>
+            </Col>
+          </Row>
+        </Container>
+        <img className="background-image-right" src={colorSharp2}/>
     </section>
   )
 }
