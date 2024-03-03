825e94af18113f813e8f9112a9113a5d738e2c6f
Jonathan Tsai <jonnsighed95@gmail.com>
2024-03-03 15:12:00 -0800

    email/contact server setup

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -49,7 +49,7 @@ export const Projects = () => {
               <h2>Projects</h2>
               <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
               <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                <Nav variant="pills" defaultActiveKey="/home">
+                <Nav variant="pills" className='nav-pills mb-5 justify-content-center align-items-center' id="pills-tab">
                   <Nav.Item>
                     <Nav.Link eventKey="first">Tab One</Nav.Link>
                   </Nav.Item>
