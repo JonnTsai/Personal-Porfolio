5949aa16a57a90926902087a94a749ebc34df1d6
Jonathan Tsai <jonnsighed95@gmail.com>
2024-03-05 15:40:10 -0800

    animations

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -49,7 +49,7 @@ export const Banner = () => {
                 <Col xs={12} md={6} xl={7}>
                     <TrackVisibility>
                     {({ isVisible }) =>
-                        <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                        <div className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                             <span className="tagline">Welcome to my Portfolio</span>
                             <h1>{'Hi Im Jonathan '} <span className='wrap'>{text}</span></h1>
                             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
