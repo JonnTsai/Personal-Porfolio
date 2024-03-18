9c4b9f59b96650cf554de87dea56e7457ef16923
Jonathan Tsai <jonnsighed95@gmail.com>
2024-03-17 22:11:11 -0700

    no zero days

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -52,7 +52,7 @@ export const Banner = () => {
                         <div className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                             <span className="tagline">Welcome to my Portfolio</span>
                             <h1>{'Hi Im Jonathan '} <span className='wrap'>{text}</span></h1>
-                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                            <p>"Software Engineer WOOOOOOO."</p>
                             <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
                         </div>}
                     </TrackVisibility>
