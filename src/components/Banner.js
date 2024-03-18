3bd6fd20b00df7e119d93e5447bbc369a1f6bb36
Jonathan Tsai <jonnsighed95@gmail.com>
2024-03-18 12:05:31 -0700

    link update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ import TrackVisibility from 'react-on-screen';
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setisDeleting] = useState(false);
-    const toRotate = [ ", a Web Designer", ", a Mobile Developer", ", a Software Engineer" ];
+    const toRotate = [ "a Web Designer", "a Mobile Developer", "a Software Engineer" ];
     const [text, setText] = useState('')
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
@@ -51,7 +51,7 @@ export const Banner = () => {
                     {({ isVisible }) =>
                         <div className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                             <span className="tagline">Welcome to my Portfolio</span>
-                            <h1>{'Hi Im Jonathan'} <span className='wrap'>{text}</span></h1>
+                            <h1>{'Hi Im Jonathan,'} <span className='wrap'>{text}</span></h1>
                             <p>In 2017, I took a leap into the world of programming on YouTube, venturing into mobile app creation and immersing myself in the captivating realm of coding and software development. 
 Fast-forward to today, and I've had the privilege of building software for a <a href="https://nnss.gov/" target="_blank">national security site</a>, a <a href="https://alexa.amazon.com/" target="_blank">prominent virtual assistant</a>, and the <a href="https://www.youtube.com/" target="_blank">leading platform for video content creation and sharing</a>, catering to millions of creators and viewers worldwide (oh how things come full circle).</p>
 <p>My main focus these days is building and finding unique solutions to incorporate AI tools into existing exosystems. I most enjoy building software that pushes the needle forward, for both the world of technology and my own professional growth.</p>
