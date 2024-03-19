4a3588e5dfe2a4e8164f7c4f4d9684bc075e125a
Jonathan Tsai <jonnsighed95@gmail.com>
2024-03-18 17:26:20 -0700

    updated Icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import { Container, Row, Col } from 'react-bootstrap'
 import MailchimpForm from './MailchimpForm'
 import logo from "../assets/img/logo.svg"
 import navIcon1 from '../assets/img/nav-icon1.svg'
-import navIcon2 from '../assets/img/nav-icon2.svg'
-import navIcon3 from '../assets/img/nav-icon3.svg'
+import githubIcon from '../assets/img/github16.svg'
+import twitterIcon from '../assets/img/twitter.svg'
 
 export const Footer = () => {
   return (
@@ -18,10 +18,10 @@ export const Footer = () => {
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/tsaij2" target="_blank"><img src={navIcon1} alt="LinkedIn Icon" /></a>
-              <a href="https://github.com/JonnTsai" target="_blank"><img src={navIcon2} alt="GitHub Icon" /></a>
-              <a href="#" target="_blank"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://github.com/JonnTsai" target="_blank"><img src={githubIcon} alt="GitHub Icon" /></a>
+              <a href="#" target="_blank"><img src={twitterIcon} alt="Icon" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2024. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react'
 import {Navbar, Container, Nav} from "react-bootstrap"
 import logo from '../assets/img/logo.svg'
 import navIcon1 from '../assets/img/nav-icon1.svg'
-import navIcon2 from '../assets/img/nav-icon2.svg'
-import navIcon3 from '../assets/img/nav-icon3.svg'
+import githubIcon from '../assets/img/github16.svg'
+import twitterIcon from '../assets/img/twitter.svg'
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -41,9 +41,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/tsaij2" target="_blank"><img src={navIcon1} alt="LinkedIn Icon" /></a>
+                <a href="https://github.com/JonnTsai" target="_blank"><img src={githubIcon} alt="GitHub Icon" /></a>
+                <a href="#" target="_blank"><img src={twitterIcon} alt="Icon" /></a>
             </div>
                 <button className="vvd"><span>Let’s Connect</span></button>
             </span>
